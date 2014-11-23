97c29533cec8befe2aa1b96e4b2603c6f660b3de
Julian Sparber <julian@sparber.net>
2014-11-23 13:08:39 +0100

    add request function for busstops

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var iconStar = "img/star.svg";
 var iconNostar = "img/notstar.svg";
 var iconSearch = "img/search.svg";
 
+
+loadBusstopsList();
+
 $(document).ready(function() {
 
   $(".bus-list").hide();
@@ -32,3 +35,50 @@ $(document).ready(function() {
   });
 
 });
+
+// cache busstops
+function loadBusstopsList() {
+	console.log("Start Request");
+  var apiUrl = "http://opensasa.info/SASAplandata/?type=BASIS_VER_GUELTIGKEIT";
+		request(apiUrl, validitySuccess, "jsonp");
+}
+
+function validitySuccess(data) {
+	if (!localStorage.version || localStorage.version != data[0].VER_GUELTIGKEIT) {
+		localStorage.clear();
+		localStorage.version = data[0].VER_GUELTIGKEIT;
+		if (!localStorage.busstops) {
+			console.log("New Data");
+			var apiUrl = "http://opensasa.info/SASAplandata/?type=REC_ORT";
+			request(apiUrl, busstopsSuccess, "jsonp");
+		}
+	}
+
+}
+
+function busstopsSuccess(data) {
+	localStorage.setItem('busstops', JSON.stringify(data));
+}
+
+// Return the busstop list as json which is stored in the localStorage
+function getBusstopList() {
+	return JSON.parse(localStorage.busstops);
+}
+
+// callback is the name of the callback arg
+function request(urlAPI, success, callback) {
+	$.ajax({
+url: urlAPI,
+dataType: 'jsonp',
+jsonp: callback,
+success: function( data ) {
+console.log("success: " + urlAPI);
+success(data);
+},
+error: function( data ) {
+console.log("Error: " + urlAPI);
+}
+});
+}
+
+
