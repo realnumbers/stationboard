9b8ba931b29346a5c51a6504fce439a61a67adf6
Julian Sparber <julian@sparber.net>
2014-12-28 19:57:03 +0100

    write each side in one div with busstop id as class

--- script.js
+++ script.js
@@ -106,6 +106,7 @@ $(".js-search").bind("input", function() {
 			downloadBoard(sug[j].busstops[i].ORT_NR);
 		}
 	}
+  drawContent();
 });
 
 //match input with busstops name and citys
@@ -135,8 +136,14 @@ function matchInput(list, input) {
 //output suggests
 function printSuggests(suggests) {
 	var favo = loadFavo();
+  var idList = new Array();
+
 	$(".search-results").empty();
 	for (var i = 0; i < suggests.length; i++) {
+    for (var j = 0; j < suggests[i].busstops.length; j++) {
+      idList[j] = suggests[i].busstops[j].ORT_NR;
+    }
+
 		var div = '<article class="station">' +
 			'<header class="station-header">' +
 			'<h1 class="station-title">' +
@@ -145,15 +152,12 @@ function printSuggests(suggests) {
 			'</h1>' +
 			'<button class="station-star nostar"></button>' +
 			'</header>' +
-			'<section class="bus-list" style="display: none;"></section>' +
+      addBoard(idList) +
 			'</article>';
-		var el = $(".search-results").append(div);
-		for (var j = 0; j < suggests[i].busstops.length; j++) {
-			var id = suggests[i].busstops[j].ORT_NR;
-		if (board[id].rides != undefined) {
-			writeBoard(el, id);
-		}
-		}
+
+			//'<section class="bus-list" style="display: none;"></section>' +
+    $(".search-results").append(div); 
+
 		//favo must be set before bindStar
 		if (favo[suggests[i].busstops[0].ORT_NR])
 			$(".search-results").find(".station-star:last").removeClass("nostar").addClass("star js-starred");
@@ -175,108 +179,127 @@ function downloadBoard(id) {
 function stationSuccess(data, id) {
 	board[id].runing = false;
 	board[id].rides = data.rides;
-	drawContent();
+	//drawContent();
+  insertRides();
 }
 
-function writeBoard(el, id) {
-	data = board[id].rides;
-	//console.log(data);
-	//var section = '<section class="bus-list" id=' + id + '></section>';
-	//el.find(".station").append(section).hide();
-	el.find(".bus-list").empty();
-	for (var i = 0; i < 3 && i < data.length; i++) {
-		//console.log("Line", i);
-		var div = '<article class="bus">' +
-			'<label class="line" style="background-color:' + data[i].hexcode + '">' +
-			(data[i].lidname).substring(0,3) +
-			'</label>' +
-			'<label class="time">' +
-			formatTime(data[i].departure) +
-			'</label>' +
-			'<label class="endstation">' +
-			data[i].last_station.split(" - ")[lang] +
-			'</label>' +
-			'</article>';
-		//console.log("Data");
-		el.find(".bus-list").append(div);
-	}
+function insertRides() {
+  /*var el = $(".search-results").append(div);
+  for (var j = 0; j < suggests[i].busstops.length; j++) {
+    var id = suggests[i].busstops[j].ORT_NR;
+    if (board[id].rides != undefined) {
+      writeBoard(el, id);
+    }
+  }
+  */
+  for (var id in board) {
+    var data = board[id].rides;
+    $("." + id).empty();
+    if (board[id].runing !== undefined && board[id].runing === false) {
+      for (var i = 0; i < data.length; i++) {
+        var div = '<article class="bus">' +
+          '<label class="line" style="background-color:' + data[i].hexcode + '">' +
+          (data[i].lidname).substring(0,3) +
+          '</label>' +
+          '<label class="time">' +
+          formatTime(data[i].departure) +
+          '</label>' +
+          '<label class="endstation">' +
+          data[i].last_station.split(" - ")[lang] +
+          '</label>' +
+          '</article>';
+        $("." + id).append(div);
+      }
 
-	if (data.length === 0) {
-		el.find(".bus-list").append(
-				'<label class="no-connections">No Connections</label>');
-	}
+      if (data.length === 0) {
+        $("." + id).append(
+            '<label class="no-connections">No Connections</label>');
+      }
+    }
+  }
+}
+
+function addBoard(idList) {
+  var div = "";
+  for (var i = 0; i < idList.length && i < 3; i++) {
+    div += '<section ' +
+      'class="' + idList[i] + 
+      ' bus-list" style="display: none;">' +
+      '</section>';
+  }
+  return div;
 }
 
 // cache busstops
 function loadBusstopsList() {
-	//console.log("Start Request");
-	var apiUrl =
-		"http://opensasa.info/SASAplandata/?type=BASIS_VER_GUELTIGKEIT";
-	request(apiUrl, validitySuccess, "jsonp");
+  //console.log("Start Request");
+  var apiUrl =
+    "http://opensasa.info/SASAplandata/?type=BASIS_VER_GUELTIGKEIT";
+  request(apiUrl, validitySuccess, "jsonp");
 }
 
 function formatTime(time) {
-	return time;
+  return time;
 }
 
 function validitySuccess(data) {
-	if (!localStorage.version || localStorage.version != data[0].VER_GUELTIGKEIT) {
-		localStorage.clear();
-		localStorage.version = data[0].VER_GUELTIGKEIT;
-		if (!localStorage.busstops) {
-			//console.log("New Data");
-			var apiUrl = "http://opensasa.info/SASAplandata/?type=REC_ORT";
-			request(apiUrl, busstopsSuccess, "jsonp");
-		}
-	}
+  if (!localStorage.version || localStorage.version != data[0].VER_GUELTIGKEIT) {
+    localStorage.clear();
+    localStorage.version = data[0].VER_GUELTIGKEIT;
+    if (!localStorage.busstops) {
+      //console.log("New Data");
+      var apiUrl = "http://opensasa.info/SASAplandata/?type=REC_ORT";
+      request(apiUrl, busstopsSuccess, "jsonp");
+    }
+  }
 
 }
 
 function busstopsSuccess(data) {
-	for (var i = 0; i < data.length; i++) {
-		// [0] is italian [1] is german
-		data[i].stop = data[i].ORT_NAME.split(" - ");
-		data[i].city = data[i].ORT_GEMEINDE.split(" - ");
-	}
-	localStorage.setItem('busstops', JSON.stringify(data));
+  for (var i = 0; i < data.length; i++) {
+    // [0] is italian [1] is german
+    data[i].stop = data[i].ORT_NAME.split(" - ");
+    data[i].city = data[i].ORT_GEMEINDE.split(" - ");
+  }
+  localStorage.setItem('busstops', JSON.stringify(data));
 }
 
 // Return the busstop list as json which is stored in the localStorage
 function getBusstopList() {
-	return JSON.parse(localStorage.busstops);
+  return JSON.parse(localStorage.busstops);
 }
 
 function loadFavo() {
-	if (localStorage.fav)
-		return JSON.parse(localStorage.fav);
-	else
-		return new Object();
+  if (localStorage.fav)
+    return JSON.parse(localStorage.fav);
+  else
+    return new Object();
 }
 
 function saveFavo(data) {
-	localStorage.fav = JSON.stringify(data);
+  localStorage.fav = JSON.stringify(data);
 }
 
 // callback is the name of the callback arg
 function request(urlAPI, success, callback, index) {
-	$.ajax({
-		url: urlAPI,
-		dataType: 'jsonp',
-		jsonp: callback,
-		success: function(data) {
-			//console.log("success: " + urlAPI);
-			success(data, index);
-		},
-		error: function(data) {
-			console.log("Error: " + urlAPI);
-		}
-	});
+  $.ajax({
+    url: urlAPI,
+    dataType: 'jsonp',
+    jsonp: callback,
+    success: function(data) {
+      //console.log("success: " + urlAPI);
+      success(data, index);
+    },
+    error: function(data) {
+      console.log("Error: " + urlAPI);
+    }
+  });
 }
 
 function drawContent() {
-	var favo = loadFavo();
-	printSuggests(sug);
-	printFavorite(favo);
-	bindToggle($(".search-results"));
-	bindToggle($(".favorites"));
+  var favo = loadFavo();
+  printSuggests(sug);
+  printFavorite(favo);
+  bindToggle($(".search-results"));
+  bindToggle($(".favorites"));
 }
